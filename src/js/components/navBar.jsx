dcf5fb00c46ab16f5f4cd99f37bae9d231b9ffd2
Joseph Nkurunziza <josetheblessing@gmail.com>
2019-08-09 14:36:44 +0200

    ft(React): implement signup and signin  - configure webpack  - Create sigin page  - Create signup page  - Fetch signup endpoint  - Fetch signin endpoint [Finishes #111]

--- src/js/components/navBar.jsx
+++ src/js/components/navBar.jsx
@@ -5,10 +5,10 @@ import '../../assets/css/home.scss'
 const NavBar = () => {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link className="navbar-brand" to="/">Home</Link>
-        <Link className="navbar-brand" to="/signin">Signin</Link>
-        <Link className="navbar-brand" to="/signup">Signup</Link>
-        <Link className="navbar-brand" to="/create-account">Create account</Link>
+        <Link className="navbar-brand nav-style" to="/">Banka</Link>
+        <Link className="navbar-brand nav-style-signin" to="/signin">Signin</Link>
+        <Link className="navbar-brand nav-style-signup" to="/signup">Signup</Link>
+        <Link className="navbar-brand nav-style-account" to="/create-account">Create account</Link>
       </nav>
     );
 }
